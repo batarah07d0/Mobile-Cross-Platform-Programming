e3af17047a461cc4f105b050cea3c9baeeb2dca9
Batara Hotdo <batarah07d0@gmail.com>
2025-02-14 12:58:52 +0700

    LabW02

--- Lab/App.js
+++ Lab/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
+import LabW02 from "./LabW02";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Batara Hotdo Horas Simbolon - 00000078626</Text>
       <StatusBar style="auto" />
+      <LabW02 />
     </View>
   );
 }
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "#ffff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
